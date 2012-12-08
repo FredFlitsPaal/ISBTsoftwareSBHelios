9262a7c88f4ffcec7c878ff75d3e985e5796483c
Wouter van Dijk <wouter@van-dijk.nu>
2012-12-08 14:34:03 +0100

    Tralalala

--- php/classes/class.pagecontroller.php
+++ php/classes/class.pagecontroller.php
@@ -9,27 +9,37 @@ class pageController{
 
     private function page($p_sPage){
         $p_sPage = explode("/", $p_sPage);
-        
+
         //Deafult subpage
         if(!isset($p_sPage[1])){
 	        $p_sPage[1] = 'index';
          }
-        
         switch($p_sPage[0]){
-            case "match":
+            case "match-scores":
                 $l_aPageCall["error"] = false;
                 //Subpage
-                if(!file_exists(PAGE_DIR.'match/' . $p_sPage[1] . '.html')){
+                if(!file_exists(PAGE_DIR.'match-scores/' . $p_sPage[1] . '.html')){
 	                echo 'De stukkels die dit gemaakt hebben hebben keihard gefaald, sla ze op hun bek dat ze het fixen!';
                 }else{
-         	       $l_aPageCall["html"] = file_get_contents(PAGE_DIR.'match/' . $p_sPage[1] . '.html');                
+         	       $l_aPageCall["html"] .= file_get_contents(PAGE_DIR.'match-scores/' . $p_sPage[1] . '.html');                
+                }
+                
+            break;
+            case "poule-information":
+                $l_aPageCall["error"] = false;
+                //Subpage
+                if(!file_exists(PAGE_DIR.'poule-information/' . $p_sPage[1] . '.html')){
+	                echo 'De stukkels die dit gemaakt hebben hebben keihard gefaald, sla ze op hun bek dat ze het fixen!';
+                }else{
+
+         	       $l_aPageCall["html"] .= file_get_contents(PAGE_DIR.'poule-information/' . $p_sPage[1] . '.html');                
                 }
                 
             break;
             
             default:
                 $l_aPageCall["error"] = true;
-                $l_aPageCall["html"] = file_get_contents(PAGE_DIR.'404.html');
+                $l_aPageCall["html"] .= file_get_contents(PAGE_DIR.'404.html');
             break;
         };
         echo json_encode($l_aPageCall);
